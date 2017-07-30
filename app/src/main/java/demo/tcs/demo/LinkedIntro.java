e6fba31f3e10de500977260cd2ffb15d6affa667
C5250435 <saswata.roy@sap.com>
2017-07-30 07:13:52 +0530

    Initial commit

---
+++ app/src/main/java/demo/tcs/demo/LinkedIntro.java
@@ -0,0 +1,13 @@
+package demo.tcs.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinkedIntro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linked_intro);
+    }
+}
---
+++ app/src/main/java/demo/tcs/demo/Utilities.java
@@ -0,0 +1,8 @@
+package demo.tcs.demo;
+
+/**
+ * Created by C5250435 on 7/30/2017.
+ */
+
+public class Utilities {
+}
